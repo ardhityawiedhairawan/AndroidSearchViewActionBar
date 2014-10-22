0c00b7e9da450a83be737f71c92a25768d59a5dc
ardhityawiedhairawan <ardhityawiedhairawan@gmail.com>
2014-10-22 13:06:56 +0700

    Initial Commit

---
+++ app/src/main/java/androlite/navigationsearch/MyActivity.java
@@ -0,0 +1,72 @@
+package androlite.navigationsearch;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity {
+
+    private TextView txtQuery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_search_results);
+
+        txtQuery = (TextView) findViewById(R.id.txtQuery);
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    /**
+     * Handling intent data
+     */
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+
+            /**
+             * nah disini kamu bisa melakukan aksi pencarian atau load data atau menampilkan data ke listview, pokoknya disini aksinya
+             */
+            txtQuery.setText("Search Query: " + query);
+
+        }
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_actions, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       return true;
+    }
+}
